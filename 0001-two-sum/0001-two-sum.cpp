20241c3d4b2807fa787dbc886466c30c91386d16
Nikhil Mahapatro <nikhilmahapatro2002@gmail.com>
2025-06-28 01:43:00 +0530

    Add files via upload

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,23 @@
 class Solution {
- public:
-  vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> numToIndex;
-
-    for (int i = 0; i < nums.size(); ++i) {
-      if (const auto it = numToIndex.find(target - nums[i]);
-          it != numToIndex.cend())
-        return {it->second, i};
-      numToIndex[nums[i]] = i;
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> m;
+                vector<int> v;
+                int n= nums.size();
+                for(int i=0;i<n;i++)
+                {
+                    
+                        int diff = target - nums[i];
+                        if(m.find(diff) != m.end())
+                        {
+                        auto p = m.find(diff);        
+                        v.push_back(p->second);
+                        v.push_back(i);
+                        }
+                        m.insert(make_pair(nums[i],i));
+                }
+          
+                return v;
     }
-
-    throw;
-  }
 };
+
--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,13 +1,4 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 class Solution {
  public:
   ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
@@ -15,7 +6,7 @@ class Solution {
     ListNode* curr = &dummy;
     int carry = 0;
 
-    while (l1 != nullptr || l2 != nullptr || carry > 0) {
+    while (l1 || l2 || carry) {
       if (l1 != nullptr) {
         carry += l1->val;
         l1 = l1->next;
