ce7a9ecf19bd81a9af1a50d3bf6afca3028ffcad
Nikhil Mahapatro <nikhilmahapatro2002@gmail.com>
2025-06-28 01:40:05 +0530

    Add files via upload

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -18,7 +18,6 @@ class Solution {
         break;
       }
 
-    // Store the information in the first row and the first column.
     for (int i = 1; i < m; ++i)
       for (int j = 1; j < n; ++j)
         if (matrix[i][j] == 0) {
@@ -26,18 +25,15 @@ class Solution {
           matrix[0][j] = 0;
         }
 
-    // Fill 0s for the matrix except the first row and the first column.
     for (int i = 1; i < m; ++i)
       for (int j = 1; j < n; ++j)
         if (matrix[i][0] == 0 || matrix[0][j] == 0)
           matrix[i][j] = 0;
 
-    // Fill 0s for the first row if needed.
     if (shouldFillFirstRow)
       for (int j = 0; j < n; ++j)
         matrix[0][j] = 0;
 
-    // Fill 0s for the first column if needed.
     if (shouldFillFirstCol)
       for (int i = 0; i < m; ++i)
         matrix[i][0] = 0;
